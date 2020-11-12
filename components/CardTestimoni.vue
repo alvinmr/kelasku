e1050be2bb2fa08e3241e026fd1fcc5f87583056
alvinmr <maulanaalvin1@gmail.com>
2020-11-13 02:04:34 +0800

    done with landing page but not yet with icon

---
+++ components/CardTestimoni.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="p-10 bg-white card-testi">
+    <div class="flex items-center">
+      <img
+        src="https://i.pravatar.cc/300"
+        class="w-3/12 rounded-full"
+        alt="avatar"
+      />
+      <div class="flex flex-col ml-10">
+        <h2 class="font-bold">Jessica Jane</h2>
+        <p>ini bintang</p>
+      </div>
+    </div>
+    <p class="mt-10">
+      Terima kasih Kelaskita. Saya sangat puas dengan pelayanannya yang kalian
+      berikan, berkat kalian anak saya semakin suka melakukan hal yang creative.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-testi {
+  width: 388px;
+  height: 330px;
+  background: #FBFBFB;
+  box-shadow: 0px 100px 80px rgba(0, 0, 0, 0.07),
+    0px 41.7776px 33.4221px rgba(0, 0, 0, 0.0503198),
+    0px 22.3363px 17.869px rgba(0, 0, 0, 0.0417275),
+    0px 12.5216px 10.0172px rgba(0, 0, 0, 0.035),
+    0px 6.6501px 5.32008px rgba(0, 0, 0, 0.0282725),
+    0px 2.76726px 2.21381px rgba(0, 0, 0, 0.0196802);
+  border-radius: 17px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,10 @@
         <HeroSection />
       </div>
       <img src="~/assets/images/wave.png" alt="wave" class="wave" />
-
-      <!-- Service Section -->
     </div>
-    <div class="pb-64 bg-white service">
-      <h1 class="text-3xl font-bold text-center">Kelasku Menyediakan</h1>
+    <!-- Service Section -->
+    <div class="pb-64 service">
+      <h1 class="text-5xl font-bold text-center">Kelasku Menyediakan</h1>
       <div class="mx-24">
         <div class="flex">
           <CardService
@@ -38,6 +37,98 @@
         </div>
       </div>
     </div>
+
+    <!-- Anak kecil lompat seneng heyy Section -->
+    <div class="relative pb-64 childern-happy">
+      <div class="flex">
+        <img
+          src="~/assets/images/kids_jump.png"
+          alt="kids jump"
+          class="w-1/2 -mt-16"
+        />
+        <div class="self-center">
+          <h1 class="text-5xl font-bold text-white">Berkembang bersama</h1>
+          <p class="w-11/12 mt-5 text-2xl text-white">
+            Di kelaskita. Kami sangat fokus terhadap anak - anak yang sedang
+            menuju ke dewasa agar memiliki creativitas, yang dimana di sekolah
+            anak - anak jarang di asah creativitasnya, dan disinilah kami hadir
+            agar membantu mengembangkan anak yang creative dan inofatif
+            kedepannya.
+          </p>
+          <div class="flex flex-row items-center mt-16">
+            <a href="#" class="mr-3 -mt-1 text-2xl text-white">Tentang Kita </a>
+            <svg
+              width="57"
+              height="29"
+              viewBox="0 0 57 29"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0)">
+                <path
+                  d="M44.0767 8.40379L42.8761 9.60437L46.832 13.5603L32.9173 13.5603L32.9173 15.2531L46.832 15.2531L42.8761 19.209L44.0767 20.4096L50.0796 14.4067L44.0767 8.40379Z"
+                  fill="white"
+                />
+              </g>
+              <rect y="13.4265" width="43.6364" height="1.67832" fill="white" />
+              <defs>
+                <clipPath id="clip0">
+                  <rect
+                    width="20.3745"
+                    height="20.3745"
+                    fill="white"
+                    transform="translate(41.6757) rotate(45)"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Mockup Section -->
+    <div class="relative pb-64 mockup">
+      <div class="flex flex-col justify-center">
+        <h1 class="mb-12 text-5xl font-bold text-center">
+          Tersedia di Berbagai Device
+        </h1>
+        <div class="w-full">
+          <img src="~assets/images/mockup.png" class="mx-auto" alt="mockup" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Testi Section -->
+    <div class="relative py-64 testi">
+      <div class="mx-24">
+        <h1 class="mb-12 text-5xl font-bold text-center">
+          Apa Yang Mereka Katakan
+        </h1>
+        <div>
+          <div class="flex justify-center mt-48">
+            <CardTestimoni />
+          </div>
+          <div class="flex justify-between mt-32">
+            <CardTestimoni class="mt-20" />
+            <CardTestimoni />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer Section -->
+    <div class="relative footer">
+      <div class="flex py-32 mx-24">
+        <div class="self-center">
+          <h1 class="text-5xl font-bold text-white">Hubungi Kami</h1>
+          <p class="text-white">
+            Untuk pertanyaan kelasku, silakan kirim email kekelasku@gmail.com
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -56,6 +147,7 @@ export default {};
   background-image: url("~assets/images/bg_awan.jpg");
   background-position: center;
   background-repeat: no-repeat;
+  background-size: cover;
 }
 
 .wave {
@@ -66,5 +158,32 @@ export default {};
   background-image: url("~assets/images/shapes/ellips.svg");
   background-repeat: no-repeat;
   background-position: right;
+}
+
+.childern-happy {
+  background-image: url("~assets/images/bg_wave_merah.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  height: 100%;
+}
+
+.mockup {
+  background-image: url("~assets/images/shapes/ellips2.svg");
+  background-repeat: no-repeat;
+  background-position: left;
+}
+
+.testi {
+  background-image: url("~assets/images/shapes/ellips2.svg");
+  background-repeat: no-repeat;
+  background-position: 800px 120px;
+}
+
+.footer {
+  background-image: url("~assets/images/bg_wave_hitam.png");
+  background-repeat: no-repeat;
+  background-position: bottom;
+  background-size: cover;
 }
 </style>
