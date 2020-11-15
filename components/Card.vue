4c3f7d7d8feca68ddf8e3de1ddd0ae9f465999bf
alvinmr <maulanaalvin1@gmail.com>
2020-11-16 00:00:39 +0800

    fix image card

--- components/Card.vue
+++ components/Card.vue
@@ -53,12 +53,6 @@ h1 {
     bg-blue-200;
 }
 
-@screen sm {
-  .card-image {
-    width: 250px;
-  }
-}
-
 .card-image {
   height: 250px;
   background-image: url("https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80");
